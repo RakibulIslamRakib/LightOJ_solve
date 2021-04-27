c2cd29bcf84e43668c1459bcbac5f6224c608279
RakibulIslamRakib <56424436+RakibulIslamRakib@users.noreply.github.com>
2021-04-27 21:34:37 +0600

    Create Hidden Secret!LOJ-1338.cpp

---
+++ Hidden Secret!LOJ-1338.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main()
+{
+  int t;cin>>t;
+     string s1,s2;
+  getline(cin,s1);
+  for(int i=1;i<=t;i++){
+    getline(cin,s1);
+    getline(cin,s2);
+    transform(s1.begin(),s1.end(),s1.begin(),:: tolower);
+    transform(s2.begin(),s2.end(),s2.begin(),:: tolower);
+    s1.erase(remove(s1.begin(),s1.end(),' '), s1.end());
+    s2.erase(remove(s2.begin(),s2.end(),' '), s2.end());
+    sort(s1.begin(),s1.end());
+    sort(s2.begin(),s2.end());
+     if (s1 == s2) {
+      cout << "Case " << i << ": Yes\n";
+    } else {
+      cout << "Case " << i << ": No\n";
+    }
+  }
+}
+
